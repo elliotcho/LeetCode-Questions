73fdfa23cc81710c3813dca8964e1e283109fb07
ycho7 <59032999+ycho7@users.noreply.github.com>
2019-12-20 15:53:06 -0500

    Update question1.java

--- question1.java
+++ question1.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] res=new int[2];
+        HashMap<Integer, Integer> map= new HashMap<>();
+        
         for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if(i!=j){
-                    if(nums[i]+nums[j]==target){
-                        res[0]=i; res[1]=j;
-                        return res;}
-                }
-            }
+           if(map.containsKey(target-nums[i])){
+               res[1]=i;
+               res[0]=map.get(target-nums[i]);
+               return res;
+           }
+            
+            
+            map.put(nums[i], i);
         }
         return res;
     }
