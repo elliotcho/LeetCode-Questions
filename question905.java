7de00d098b68fd6a6727fdab2fc5c9d08cb01b25
ycho7 <59032999+ycho7@users.noreply.github.com>
2019-12-20 16:00:10 -0500

    Update question905.java

--- question905.java
+++ question905.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] sortArrayByParity(int[] A) {
-        ArrayList<Integer> even=new ArrayList<>();    
-        ArrayList<Integer> odd=new ArrayList<>();
-        int[] res= new int[A.length];
-        
+        int mark=0;
         for(int i=0;i<A.length;i++){
-            if(A[i]%2==0){even.add(A[i]);}
-            else{odd.add(A[i]);}  
+            if(A[i]%2==0){
+                int temp=A[mark];
+                A[mark]=A[i];
+                A[i]=temp;
+                mark++;
+            }
+            
+            
         }
         
-        int k=0;
-        for(int i=0; i<even.size();i++){res[k]=even.get(i);k++;}
-        for(int j=0;j<odd.size();j++){res[k]=odd.get(j); k++;}
-        
-        
-        
-        return res;
+        return A;
     }
 }
