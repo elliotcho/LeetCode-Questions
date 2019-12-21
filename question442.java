d6100ab32a41a3924f5fa7e11bff855108310954
ycho7 <59032999+ycho7@users.noreply.github.com>
2019-12-20 21:40:10 -0500

    Add files via upload

---
+++ question442.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> dup=new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){dup.add(nums[i]);}
+        }
+        
+        
+        return dup;
+    }
+}
