7f459d6aef0720a30a8e83070d01ccc20b7b0239
ycho7 <59032999+ycho7@users.noreply.github.com>
2019-12-20 22:38:34 -0500

    Add files via upload

---
+++ question1287.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int percent=arr.length/4;
+        int res=0, count=0;
+        
+        if(arr.length==1){return arr[0];}
+        
+        for(int i=1;i<arr.length;i++){
+            if(arr[i-1]==arr[i]){
+                count++;
+                if(count>percent){return arr[i];}
+            }
+            else{
+                count=0;
+                res=arr[i];
+            }
+        }
+        
+        
+        
+        return res;
+    }
+}
