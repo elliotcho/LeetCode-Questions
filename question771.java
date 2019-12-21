d87342a55dbddfcf766158a906aa5238135ef3b2
ycho7 <59032999+ycho7@users.noreply.github.com>
2019-12-21 13:06:16 -0500

    Add files via upload

---
+++ question771.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        HashSet<Character> set=new HashSet<>();
+        int result=0;
+        
+        for(int i=0;i<J.length();i++){
+            set.add(J.charAt(i));
+        }
+        
+        for(int j=0;j<S.length();j++){
+            if(set.contains(S.charAt(j))){
+                result++;
+            }
+        }
+        
+        return result;
+    }
+}
