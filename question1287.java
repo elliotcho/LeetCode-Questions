2044bb13c25e1da0ccdc4e7f5ad8ff2b1ce080bc
ycho7 <59032999+ycho7@users.noreply.github.com>
2019-12-20 22:40:19 -0500

    Update question1287.java

--- question1287.java
+++ question1287.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
         int percent=arr.length/4;
-        int res=0, count=0;
+        int res=0, count=1;
         
         if(arr.length==1){return arr[0];}
         
@@ -10,6 +10,7 @@ class Solution {
                 count++;
                 if(count>percent){return arr[i];}
             }
+            
             else{
                 count=0;
                 res=arr[i];
