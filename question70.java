0bf78a7570376e3773ff3d8255e983ed02b70906
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-03 01:39:22 -0500

    Add files via upload

---
+++ question70.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] table=new int[n+1];
+        table[1]=1;
+
+        if(n>=2){table[2]=2;}
+        
+        for(int i=3; i<=n;i++){
+            table[i]=table[i-1]+table[i-2];
+        }
+        
+        return table[n];
+    }
+}
