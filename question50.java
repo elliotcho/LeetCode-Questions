37fb062ee5a452542decefcd1a038ebacad5c51d
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-03 17:44:14 -0500

    Add files via upload

---
+++ question50.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n>=0){
+            if(n==0){return 1;}
+            
+            if(n%2==0){
+                double temp=myPow(x, n/2);
+                return temp*temp;
+            }
+            
+            else{
+                return x*myPow(x, n-1);
+            }
+        }
+        
+        else{
+            if(n%2==0){
+                double temp=myPow(x, n/2);
+                return temp*temp;
+            }
+            
+            return (1/x)*myPow(x, n+1);
+        }
+    }
+}
