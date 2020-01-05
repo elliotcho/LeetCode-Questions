edf0469d1b907d0b42a2fd62f1ca80842e4bf81b
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-04 22:49:54 -0500

    Add files via upload

---
+++ question1137.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] table=new int[n+1];
+        
+        if(n==0){return 0;}
+        if(n==1){return 1;}
+        if(n==2){return 1;}
+        
+        table[0]=0; table[1]=1; table[2]=1;
+        
+        for(int i=3;i<table.length;i++){
+            table[i]=table[i-1]+table[i-2]+table[i-3];
+        }
+        
+        return table[n];
+    }
+}
