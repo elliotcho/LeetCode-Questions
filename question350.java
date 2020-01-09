1c9357cc326579863ca6e02bfb4dbba61f262ae1
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-09 00:59:47 -0500

    Add files via upload

---
+++ question350.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map=new HashMap<>();
+        ArrayList<Integer> intersect=new ArrayList<>();
+        
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i])){
+                map.put(nums1[i], map.get(nums1[i])+1);    
+            }
+            
+            else{map.put(nums1[i],1);}
+        }
+        
+        for(int j=0;j<nums2.length;j++){
+            if(map.containsKey(nums2[j]) && map.get(nums2[j])!=0){
+                intersect.add(nums2[j]);
+                map.put(nums2[j], map.get(nums2[j])-1);    
+            }
+        }
+        
+        int[] res=new int[intersect.size()];
+        for(int k=0;k<intersect.size();k++){
+            res[k]=intersect.get(k);
+        }
+        
+        return res;
+    }
+}
