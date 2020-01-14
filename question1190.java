5b810062d9aab5a7e959e2132b73d0d3f66427a8
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-13 20:19:19 -0500

    Add files via upload

---
+++ question1190.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<StringBuilder> stack=new Stack<>();
+        StringBuilder res=new StringBuilder();
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+               stack.push(res);
+               res=new StringBuilder();
+            }
+            
+            else if(s.charAt(i)==')'){
+                res=res.reverse();
+                
+                String prepend=stack.pop().toString();
+                
+                for(int j=prepend.length()-1;j>=0;j--){
+                    res.insert(0,prepend.charAt(j));
+                }
+            }
+                  
+            else{
+                res.append(s.charAt(i));
+            }
+            
+        }
+     
+        return res.toString();
+    }
+}
