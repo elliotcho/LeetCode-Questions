961b4a80164e6d4a49dadd713a531712d6bd4493
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-18 19:28:04 -0500

    Add files via upload

---
+++ question532.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        if(k<0){return 0;}
+  
+        HashSet<Integer> allNums=new HashSet<>();
+        HashSet<Integer> pairs=new HashSet<>();
+        
+        for(int j=0;j<nums.length;j++){
+            if(allNums.contains(nums[j]-k)){
+                pairs.add(nums[j]-k);
+            }
+            
+            if(allNums.contains(nums[j]+k)){
+                pairs.add(nums[j]);
+            }
+            
+            
+            allNums.add(nums[j]);
+        }
+        
+        return pairs.size();
+    }
+}
