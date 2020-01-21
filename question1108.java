fcd448933688df94466975e74f81caeec77ce45b
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-20 20:52:02 -0500

    Add files via upload

---
+++ question1108.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder res=new StringBuilder();
+        
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                res.append('[');
+                res.append('.');
+                res.append(']');
+            }
+            
+            else{
+                res.append(address.charAt(i));
+            }
+        }
+        
+        return res.toString();
+    }
+}
