81e2e6585ce830ebcda8c18a296ff1bab3cb2742
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-21 17:57:51 -0500

    Add files via upload

---
+++ question110.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){return true;}
+        
+        int left=height(root.left);
+        int right=height(root.right);
+        
+        return Math.abs(left-right)<2 && isBalanced(root.left) 
+            && isBalanced(root.right);
+    }
+    
+    public int height(TreeNode root){
+        if(root==null){return 0;}
+        return Math.max(height(root.left), height(root.right))+1;
+    }
+}
