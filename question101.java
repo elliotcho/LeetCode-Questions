75f16ae8668b52722f9fd17f4460652e708b15a0
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-21 21:01:06 -0500

    Add files via upload

---
+++ question101.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+       return helper(root, root);
+    }
+    
+    public boolean helper(TreeNode node1, TreeNode node2){
+        if(node1==null && node2==null){return true;}
+        if(node1==null || node2==null){return false;}
+        
+        return node1.val==node2.val 
+            && helper(node1.left, node2.right) && helper(node1.right, node2.left);
+    }
+}
