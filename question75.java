944dafa349ab2fc42fec489fcb4607cc5e02e8fc
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-27 14:46:30 -0500

    Add files via upload

---
+++ question75.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int start=0, end=nums.length-1, index=0;
+       
+        while(index<=end){
+            if(nums[index]==0){
+                nums[index]=nums[start];
+                nums[start]=0;
+                start++; 
+                index++;
+            }
+            
+            else if(nums[index]==2){
+                nums[index]=nums[end];
+                nums[end]=2;
+                end--;
+            }
+            
+            else{
+                index++;
+            }
+        }
+        
+        return;
+    }
+}
