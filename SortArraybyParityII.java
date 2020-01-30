98d607818c914a9784d651fdcfd9e2749400cdc9
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-30 00:27:53 -0500

    Update and rename question922.java to SortArraybyParityII.java

--- SortArraybyParityII.java
+++ SortArraybyParityII.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] sortArrayByParityII(int[] A) {
-   
         int[] res= new int[A.length];
+       
         int eI=0, oI=1;
-        
         
         for(int i=0;i<A.length;i++){
             if(A[i]%2==0){res[eI]=A[i]; eI+=2;}
