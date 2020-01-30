7be15afc4a195ce8f52c5a41f4e6e819972a070a
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-01-30 00:35:43 -0500

    Update and rename question938.java to RangeSumofBST.java

--- RangeSumofBST.java
+++ RangeSumofBST.java
@@ -8,7 +8,6 @@
  * }
  */
 class Solution {
-  
     public int rangeSumBST(TreeNode root, int L, int R) {
         if(root==null){return 0;}
 	if(root.val<L){return rangeSumBST(root.right, L, R);}
@@ -16,9 +15,4 @@ class Solution {
         
         return root.val+rangeSumBST(root.right,L, R)+rangeSumBST(root.left,L,R);
     }
-    
-
-   
-    
-
 }
