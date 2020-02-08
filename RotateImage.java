93440094e80495f402f8acf0bad2ff496039c5f5
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-02-08 01:46:33 -0500

    Add files via upload

---
+++ RotateImage.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int[] row: matrix){
+            int start=0, end=row.length-1;
+            
+            while(start<end){
+                swap(start++, end--, row);
+            }
+        }
+        
+        return;
+    }
+    
+    public void swap(int i, int j, int[] arr){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
