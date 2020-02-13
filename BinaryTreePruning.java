0615ac472bd3be52ea0a7ee85f97f4f0c09042ce
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-02-13 01:42:22 -0500

    Add files via upload

---
+++ BinaryTreePruning.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        containsOne(root);
+        return root;
+    }
+    
+    public boolean containsOne(TreeNode root){
+        if(root==null){return false;}
+        
+        boolean left=containsOne(root.left);
+        boolean right=containsOne(root.right);
+        
+        if(!left){root.left=null;}
+        if(!right){root.right=null;}
+        
+        return root.val==1 || left || right;
+    }
+}
