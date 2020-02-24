afc5c6fc8df468f001e0e33cbb28bf46963961fd
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-02-24 00:30:25 -0500

    Add files via upload

---
+++ BattleshipsinaBoard.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+        int res=0;
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                if(board[i][j]=='.'){continue;}
+                
+                if(j>0 && board[i][j-1]=='X'){continue;}
+                
+                if(i>0 && board[i-1][j]=='X'){continue;}
+                
+                res++;
+            }
+        }
+        
+        return res;
+    }
+}
