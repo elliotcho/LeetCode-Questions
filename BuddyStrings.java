2eb65eb28c286e59536f9450ef19e10f4312db1a
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-02-27 01:22:34 -0500

    Add files via upload

---
+++ BuddyStrings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean buddyStrings(String A, String B) {
+        if(A.length() != B.length()){return false;}
+        
+        if(A.equals(B)){
+            HashSet<Character> set=new HashSet<>();
+            
+            for(char c: A.toCharArray()){set.add(c);}
+            
+            return set.size()<A.length()? true: false;
+        }
+        
+        else{
+            ArrayList<Integer> diff=new ArrayList<>();
+        
+            for(int i=0;i<A.length();i++){
+                if(A.charAt(i)!=B.charAt(i)){diff.add(i);}
+            }
+        
+            if(diff.size()==2 && 
+                A.charAt(diff.get(0))==B.charAt(diff.get(1))&&
+                A.charAt(diff.get(1))==B.charAt(diff.get(0))){
+                    return true;
+            }
+             
+            return false;
+        }
+    }
+}
