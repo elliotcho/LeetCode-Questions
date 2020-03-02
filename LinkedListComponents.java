d5e280634e25c2c7d58b48dc4f4053eb5df5d32d
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-03-02 02:24:57 -0500

    Add files via upload

---
+++ LinkedListComponents.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] G) {
+       HashSet<Integer> set=new HashSet<>();
+        
+       for(int i: G){set.add(i);}
+        
+       int res=0;
+       
+       while(head!=null){
+           if(set.contains(head.val) && 
+              (head.next==null || !set.contains(head.next.val))){
+               res++;
+           }
+           
+           head=head.next;
+       }
+        
+       return res;
+    }
+}
