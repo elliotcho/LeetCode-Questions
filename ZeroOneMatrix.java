da3fe6f6337e2c9302df4789c5fc5fbf245292a0
elliotcho7 <59032999+elliotcho7@users.noreply.github.com>
2020-03-24 15:07:05 -0400

    Add files via upload

---
+++ ZeroOneMatrix.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] updateMatrix(int[][] matrix) {
+        int[] rowDirections={-1, 1, 0, 0};
+        int[] colDirections={0, 0, -1, 1};
+        
+        LinkedList<Integer> rowQueue=new LinkedList<>();
+        LinkedList<Integer> colQueue=new LinkedList<>();
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    rowQueue.add(i);
+                    colQueue.add(j);
+                }
+                
+                else{
+                    matrix[i][j]=Integer.MAX_VALUE;
+                }
+            }
+        }
+        
+        while(rowQueue.size()!=0 && colQueue.size()!=0){
+            int currRow=rowQueue.remove();
+            int currCol=colQueue.remove();
+            int currPos=matrix[currRow][currCol];
+            
+            for(int i=0;i<4;i++){
+                int row=currRow+rowDirections[i];
+                int col=currCol+colDirections[i];
+                
+                if(row>=0 && row<matrix.length && col>=0 && col<matrix[0].length && matrix[row][col]>currPos+1){
+                    matrix[row][col]=currPos+1;
+                    rowQueue.add(row);
+                    colQueue.add(col);
+                }
+            }
+        }
+        
+        return matrix;
+    }
+}
