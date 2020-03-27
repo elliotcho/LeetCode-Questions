4e2ecacf0f76a3a11cad8566c1c98059a6c029df
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-03-27 16:31:22 -0400

    Add files via upload

---
+++ ThreeSum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        ArrayList<List<Integer>> res=new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length-2;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                int start=i+1;
+                int end=nums.length-1;
+                int target=0-nums[i];
+                
+                while(start<end){
+                    int sum=nums[start]+nums[end];
+                    
+                    if(sum==target){
+                        res.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                        
+                        while(start<end && nums[start]==nums[start+1]){start++;}
+                        while(start<end && nums[end]==nums[end-1]){end--;}
+                    
+                        start++;
+                        end--;
+                    }
+                    
+                    else if(sum>target){end--;}
+                    
+                    else{start++;}
+                }
+            }
+        }
+        
+        return res;
+    }
+}
