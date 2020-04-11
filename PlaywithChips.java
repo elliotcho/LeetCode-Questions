121f233a7d8d31dad1571f318d8700c9338b1408
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-04-11 03:40:48 -0400

    Add files via upload

---
+++ PlaywithChips.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostToMoveChips(int[] chips) {
+        int odd=0;
+        int even=0;
+        
+        for(int i: chips){
+            if(i%2==0){even++;}
+            else{odd++;}
+        }
+        
+        return Math.min(even, odd);
+    }
+}
