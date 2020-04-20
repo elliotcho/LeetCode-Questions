fa1b8cf04ef7c1d572ae14dfae360e82371df660
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-04-20 16:22:35 -0400

    Add files via upload

---
+++ SingleElementinaSortedArray.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(mid>0 && nums[mid]==nums[mid-1]){
+                if((mid-1)%2==0){
+                    start=mid+1;
+                }
+                
+                else{
+                    end=mid-1;
+                }
+            }
+            
+            else if(mid<nums.length-1 && nums[mid]==nums[mid+1]){
+                if((mid+1)%2==0){
+                    end=mid-1;
+                }
+                
+                else{
+                    start=mid+1;
+                }
+            }
+            
+            else{
+                return nums[mid];
+            }
+        }
+        
+        return -1;
+    }
+}
