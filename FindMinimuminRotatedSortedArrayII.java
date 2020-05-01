42b1674123f71006204243d9039c5a4fdef01454
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-05-01 18:24:05 -0400

    Add files via upload

---
+++ FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        
+        int start=0, end=n-1;
+        
+        while(start<end){
+            int mid=start+(end-start)/2;
+            
+            if(nums[mid]>nums[end]){
+                start=mid+1;
+            }
+            
+            else if(nums[mid]<nums[end]){
+                end=mid;
+            }
+            
+            else{
+                end--;
+            }
+        }
+        
+        return nums[end];
+    }
+}
