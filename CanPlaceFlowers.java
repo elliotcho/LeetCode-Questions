d54ac7a3ed838a9293cb974529ba372cfe1c9ffa
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-05-10 00:06:06 -0400

    Add files via upload

---
+++ CanPlaceFlowers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i=0;i<flowerbed.length;i++){
+            if(i>0 && flowerbed[i-1]==1){continue;}
+            
+            if(i<flowerbed.length-1 && flowerbed[i+1]==1){continue;}
+            
+            if(flowerbed[i]==1){continue;}
+            
+            flowerbed[i]=1;
+            n--;
+        }
+        
+        return n<=0;
+    }
+}
