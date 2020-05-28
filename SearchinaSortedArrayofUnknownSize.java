a945fc26804bdb9bd210ffd5c68213f5f29a8efe
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-05-28 01:21:20 -0400

    Add files via upload

---
+++ SearchinaSortedArrayofUnknownSize.java
@@ -0,0 +1,36 @@
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     public int get(int index) {}
+ * }
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int start=0, end=1;
+        
+        while(reader.get(end)<target){
+            start=end;
+            end*=2;
+        }
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(reader.get(mid)==target){
+                return mid;
+            }
+            
+            else if(reader.get(mid)>target){
+                end--;
+            }
+            
+            else{
+                start++;
+            }
+        }
+        
+        return -1;
+    }
+}
