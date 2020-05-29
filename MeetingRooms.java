4fee1b0c14c91fc877ee64adaad24f7279d23aa8
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-05-29 03:07:55 -0400

    Add files via upload

---
+++ MeetingRooms.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        int[] start=new int[intervals.length];
+        int[] end=new int[intervals.length];
+        
+        for(int i=0;i<intervals.length;i++){
+            start[i]=intervals[i][0];
+            end[i]=intervals[i][1];
+        }
+        
+        Arrays.sort(start);
+        Arrays.sort(end);
+        
+        for(int i=1;i<intervals.length;i++){
+            if(start[i]<end[i-1]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
