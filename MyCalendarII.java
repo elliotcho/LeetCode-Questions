1603068e24bfc7e8c1fe2962f02d581731f90e78
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-05-30 00:51:54 -0400

    Add files via upload

---
+++ MyCalendarII.java
@@ -0,0 +1,31 @@
+class MyCalendarTwo {
+    TreeMap<Integer, Integer> map;
+    
+    public MyCalendarTwo() {
+        map=new TreeMap<>();
+    }
+    
+    public boolean book(int start, int end) {
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) -1);
+        
+        int bookings=0;
+        
+        for(int v: map.values()){
+            bookings+=v;
+            
+            if(bookings>2){
+                map.put(start, map.get(start)-1);
+                map.put(end, map.get(end) + 1);
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
