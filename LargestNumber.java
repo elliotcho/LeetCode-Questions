3c4e9d7cba2289465f8162fd3aa563f7491aa6c5
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-07-08 00:26:23 -0600

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        StringBuilder res = new StringBuilder();
+        String[] strs = new String[nums.length];
+        
+        for(int i=0;i<strs.length;i++){
+            strs[i] = String.valueOf(nums[i]);
+        }
+        
+        Arrays.sort(strs, (s1, s2) -> (s2+s1).compareTo(s1+s2));
+        
+        for(String s: strs){res.append(s);}
+        
+        return res.charAt(0) =='0'? "0": res.toString();
+    }
+}
