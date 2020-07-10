6b40da1dfa301f26e00203a2dbca35c8c25609f5
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-07-10 00:05:29 -0600

    Add files via upload

---
+++ NestedListWeightSum.java
@@ -0,0 +1,62 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        int res = 0;
+        int level = 1;
+        
+        LinkedList<NestedInteger> queue = new LinkedList<>();
+        
+        for(NestedInteger element: nestedList){
+            queue.add(element);
+        }
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            
+            for(int i=0;i<size;i++){
+                NestedInteger currElement = queue.remove();
+                
+                if(currElement.isInteger()){
+                    res+=level*currElement.getInteger();
+                }
+                
+                else{
+                    for(NestedInteger element: currElement.getList()){
+                        queue.add(element);
+                    }
+                }
+            }
+            
+            level++;
+        }
+        
+        return res;
+    }
+}
